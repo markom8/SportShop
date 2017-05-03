335c3e8aa25c609aab6a8fbe6a5fa93a7c924665
markom8 <milovanovic.m85@gmail.com>
2017-05-03 21:57:43 +0200

    A

--- src/main/java/configurations/MyWebInitializer.java
+++ src/main/java/configurations/MyWebInitializer.java
@@ -1,5 +1,21 @@
 package configurations;
 
-public class MyWebInitializer {
+public class MyWebInitializer extends
+AbstractAnnotationConfigDispatcherServletInitializer {
+
+@Override
+protected Class<?>[] getRootConfigClasses() {
+return new Class[] { SpringRootConfig.class };
+}
+
+@Override
+protected Class<?>[] getServletConfigClasses() {
+return new Class[] { SpringWebConfig.class };
+}
+
+@Override
+protected String[] getServletMappings() {
+return new String[] { "/" };
+}
 
 }
